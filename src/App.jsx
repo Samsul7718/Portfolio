f78415e7ec4d2747390eea4f2f41d766a76568c9
samsul alam <samsul7718@gmail.com>
2025-07-25 22:48:45 +0530

    banner created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from "./components/navbar/Navbar"
+import Banner from './components/banner/Banner'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
    <div className="w-full h-auto bg-bodyColor text-lightText font-bodyFont">
      
     <Navbar />
+    <Banner/>
    </div>
        
       
---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section
+    id='home'
+        className="w-full px-4 sm:px-6 lg:px-16  pt-10 pb-20 border-b-[1px] 
+      font-titleFont border-b-black overflow-hidden"
+    >
+        <div>
+
+        </div>
+    </section>
+  )
+}
+
+export default Banner
