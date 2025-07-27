90b5d6cc5a1d8f348c1fc18f64d47f6b7d206d6a
samsul alam <samsul7718@gmail.com>
2025-07-27 17:36:44 +0530

    file modified

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,5 +1,5 @@
 import logo from './images/Untitled design.png';
-import portfolio from './images/Occupation.png';
+import portfolio from './images/port.png';
 
 
 
--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <section
     id='home'
-        className="w-full px-4 sm:px-6 lg:px-16  pt-32 pb-20 border-b-[1px] 
+        className="w-full px-4 sm:px-6 lg:px-16  pt-26 pb-20 border-b-[1px] 
       font-titleFont border-b-black overflow-hidden"
     >
         <div  className="flex w-full max-w-screen-2xl mx-auto">
