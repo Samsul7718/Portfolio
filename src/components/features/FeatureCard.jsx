fc9605e65c3f861d942b57460e409a8b321af936
samsul alam <samsul7718@gmail.com>
2025-07-29 22:29:32 +0530

    features card updated

--- src/components/features/FeatureCard.jsx
+++ src/components/features/FeatureCard.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const FeatureCard = () => {
+const FeatureCard = ({icon,title,des}) => {
   return (
-    <div>FeatureCard</div>
+    <div className='w-full px-12 h-80 py-10 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-r
+ from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-black hover:to-[#1e2024] transition-colors
+ duration-100 group'>
+      <div className='h-72 overflow-y-hidden'>
+        <div className='flex h-full flex-col gap-8 translate-y-14 group-hover:translate-y-0 transition-transform duration-500'>
+          <div>
+             <span className="text-5xl text-designColor">{icon}</span>
+          </div>
+          <div>
+            <h2>{title}</h2>
+            <p>{des}</p>
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
