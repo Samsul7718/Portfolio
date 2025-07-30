1717bc5993768367e47ad88a5f3a258836917c68
samsul alam <samsul7718@gmail.com>
2025-07-30 23:45:55 +0530

    card added

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Title from '../layouts/Title';
+import FeatureCard from './FeatureCard';
+import { SiSemanticweb } from 'react-icons/si';
 
 const Features = () => {
   return (
@@ -7,6 +9,50 @@ const Features = () => {
      id="features"
       className="w-full pt-10 py-20 border-b-[1px] border-b-black">
         <Title title="Features" des="What i Do" />
+        <div>
+          <FeatureCard
+          title="Web Development"
+          des="Building modern, responsive, and high-performance web applications using the latest technologies
+           like React, JavaScript, HTML, CSS, and more."
+          icon={<SiSemanticweb />}
+          />
+          <FeatureCard
+          title="App Development"
+          des="Designing and developing high-performance mobile and web applications using modern frameworks
+           like React Native, Flutter, and Progressive Web Apps (PWA)."
+          icon={<AiFillAppstore />}
+          />
+          <FeatureCard
+          title="Web Development"
+          des="Building modern, responsive, and high-performance web applications using the latest technologies
+           like React, JavaScript, HTML, CSS, and more."
+          icon={<SiSemanticweb />}
+          />
+          <FeatureCard
+          title="Web Development"
+          des="Building modern, responsive, and high-performance web applications using the latest technologies
+           like React, JavaScript, HTML, CSS, and more."
+          icon={<SiSemanticweb />}
+          />
+          <FeatureCard
+          title="Web Development"
+          des="Building modern, responsive, and high-performance web applications using the latest technologies
+           like React, JavaScript, HTML, CSS, and more."
+          icon={<SiSemanticweb />}
+          />
+          <FeatureCard
+          title="Web Development"
+          des="Building modern, responsive, and high-performance web applications using the latest technologies
+           like React, JavaScript, HTML, CSS, and more."
+          icon={<SiSemanticweb />}
+          />
+          <FeatureCard
+          title="Web Development"
+          des="Building modern, responsive, and high-performance web applications using the latest technologies
+           like React, JavaScript, HTML, CSS, and more."
+          icon={<SiSemanticweb />}
+          />
+        </div>
 
     </section>
   )
