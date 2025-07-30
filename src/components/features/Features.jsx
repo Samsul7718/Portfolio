0702fe108fcda1f785b3681b4807cccc26d79fa4
samsul alam <samsul7718@gmail.com>
2025-07-30 23:50:38 +0530

    card completed

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Title from '../layouts/Title';
 import FeatureCard from './FeatureCard';
-import { SiProgress, SiSemanticweb } from 'react-icons/si';
+import { SiAntdesign, SiProgress, SiSemanticweb } from 'react-icons/si';
 import { AiFillAppstore } from 'react-icons/ai';
+import { FaGlobe, FaMobile } from 'react-icons/fa';
 
 const Features = () => {
   return (
@@ -30,29 +31,24 @@ const Features = () => {
           icon={<SiProgress />}
           />
           <FeatureCard
-          title="Web Development"
-          des="Building modern, responsive, and high-performance web applications using the latest technologies
-           like React, JavaScript, HTML, CSS, and more."
-          icon={<SiSemanticweb />}
+          title="Mobile Development"
+          des="Creating high-performance, cross-platform mobile applications using React Native and Flutter, 
+          ensuring seamless user experiences on iOS and Android devices."
+          icon={<FaMobile />}
           />
           <FeatureCard
-          title="Web Development"
-          des="Building modern, responsive, and high-performance web applications using the latest technologies
-           like React, JavaScript, HTML, CSS, and more."
-          icon={<SiSemanticweb />}
+         title="UX Design"
+          des="Crafting intuitive and engaging user experiences through research-driven design, wireframing,
+           prototyping, and usability testing for web and mobile applications."
+          icon={<SiAntdesign />}
           />
           <FeatureCard
-          title="Web Development"
-          des="Building modern, responsive, and high-performance web applications using the latest technologies
-           like React, JavaScript, HTML, CSS, and more."
-          icon={<SiSemanticweb />}
+           title="Hoisting Websites"
+          des="Providing reliable and secure web hosting solutions with fast performance, domain management, 
+          and scalable server options for seamless website deployment."
+          icon={<FaGlobe />}
           />
-          <FeatureCard
-          title="Web Development"
-          des="Building modern, responsive, and high-performance web applications using the latest technologies
-           like React, JavaScript, HTML, CSS, and more."
-          icon={<SiSemanticweb />}
-          />
+         
         </div>
 
     </section>
