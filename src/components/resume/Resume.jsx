5e40e5caf518639bd2505af7002233d136e77066
samsul alam <samsul7718@gmail.com>
2025-08-03 23:09:21 +0530

    all skill initialize with useState

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Title from '../layouts/Title'
 
 const Resume = () => {
+    const [educationData, setEducationData] = useState(true);
+  const [skillData, setSkillData] = useState(false);
+  const [experienceData, setExperienceData] = useState(false);
+  const [achievementData, setAchievementData] = useState(false);
   return (
     <section id='resume' className='w-full py-20 border-b-[1px] border-b-black'>
         <div className='flex justify-center items-center text-center '>
