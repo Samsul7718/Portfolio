d09ba4b70c9c7593e73b12c82e44b4971b09d1fb
samsul alam <samsul7718@gmail.com>
2025-08-05 23:19:23 +0530

    resume card modifying

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,13 @@ function App() {
    <div className="w-full h-auto bg-bodyColor text-lightText font-bodyFont">
      
     <Navbar />
-    <Banner/>
-    <Features/>
-    <Project/>
-    <Resume/>
+    <div max-w-screen-2xl mx-auto px-4 sm:px-0 md:px-6 lg:px-16>
+       <Banner/>
+       <Features/>
+       <Project/>
+       <Resume/>
+    </div>
+    
    </div>
        
       
--- src/components/resume/Education.jsx
+++ src/components/resume/Education.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import ResumeCard from './ResumeCard'
 
 const Education = () => {
   return (
-    <div>Education</div>
+    <div>
+      <div>
+         <div className=''>
+            <p className="text-sm text-designColor tracking-[4px]">2012-2018</p>
+          <h2 className="text-4xl font-bold">Education Quality</h2>
+         </div>
+         <div  className="w-full mt-14 h-[1000px] border-l-[6px] border-l-black
+      border-opacity-30 flex flex-col gap-10">
+            <ResumeCard/>
+            <ResumeCard/>
+            <ResumeCard/>
+            <ResumeCard/>
+            <ResumeCard/>
+           {/* <ResumeCard
+            title="Front-end-developer"
+            subTitle="CODING NINJA (2022-2023)"
+            result="React"
+            des="I learned the coding language java then i move forword to HTML,CSS,JS,React.Apart from 
+            this i made some project.Now I'm upgrading my self."
+          /> */}
+         </div>
+      </div>
+      <div>
+         <div></div>
+         <div></div>
+      </div>
+    </div>
   )
 }
 
