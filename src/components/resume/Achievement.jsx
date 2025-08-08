412414e6715223c0a7f34e2b9783c22ef8fbda1f
samsul alam <samsul7718@gmail.com>
2025-08-08 19:01:31 +0530

    experience added

--- src/components/resume/Achievement.jsx
+++ src/components/resume/Achievement.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ResumeCard from './ResumeCard'
 
 const Achievement = () => {
   return (
@@ -8,7 +9,27 @@ const Achievement = () => {
           <p className="text-sm text-designColor tracking-[4px]">Features</p>
           <h2 className="text-4xl font-bold">Design Experience </h2>
         </div>
-        <div></div>
+        <div  className="w-full mt-14 h-[1000px] border-l-[6px] border-l-black
+      border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="Photoshot skill "
+            subTitle="SELF-IMAGE (2018-2022)"
+            result="5.4/10"
+            des="I learned photoshot for self photography and selfies, and trying to modify the image quality by own."
+          />
+          <ResumeCard
+            title="Figma design"
+            subTitle="self-learning (2024)"
+            result="3.9/10"
+            des="I am trying to improve my figma design to build the proper development skill from yt and doc"
+          />
+          <ResumeCard
+            title="Canva skill"
+            subTitle="self learning (2024-25)"
+            result="4.5/10"
+            des="I learne d the canva design skill for my own social media post. to get more audience to engaged my skill."
+          />
+        </div>
       </div>
     </div>
   )
