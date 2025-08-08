1e3de91c3995e941e44ac28f49201128f184c204
samsul alam <samsul7718@gmail.com>
2025-08-08 18:51:02 +0530

    acheivements heaader added

--- src/components/resume/Achievement.jsx
+++ src/components/resume/Achievement.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Achievement = () => {
   return (
-    <div>Achievement</div>
+    <div>
+      <div>
+        <div className="py-12 font-titleFont">
+          <p className="text-sm text-designColor tracking-[4px]">Features</p>
+          <h2 className="text-4xl font-bold">Design Experience </h2>
+        </div>
+        <div></div>
+      </div>
+    </div>
   )
 }
 
--- src/components/resume/ResumeCard.jsx
+++ src/components/resume/ResumeCard.jsx
@@ -7,7 +7,7 @@ const ResumeCard = ({title,subTitle,result,des}) => {
             <span className="absolute w-5 h-5 rounded-full -top-2 -left-3 flex justify-center 
         items-center bg-green bg-opacity-60">
           <span className="w-3 h-3 rounded-full bg-red inline-flex
-          group-hover:bg-designColor duration-300"></span>
+          group-hover:bg-green-600 duration-300"></span>
          </span>
           </div>
           <div  
