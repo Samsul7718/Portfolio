d99157b31a87e891b94dedb54f144623f6367502
samsul alam <samsul7718@gmail.com>
2025-08-19 00:08:39 +0530

    left contact file created

--- src/components/contact/ContactLeft.jsx
+++ src/components/contact/ContactLeft.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const ContactLeft = () => {
   return (
-    <div>ContactLeft</div>
+    <div
+     className="w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] shadow-shadowOne
+                    p-8 rounded-lg  flex flex-col gap-8 justify-center"
+    >
+      <img src="" alt="" />
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
