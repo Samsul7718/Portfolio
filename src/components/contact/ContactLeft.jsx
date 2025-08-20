673cc9649190a10c09ef49b839a219cae692e741
samsul alam <samsul7718@gmail.com>
2025-08-20 16:15:37 +0530

    working on contact page

--- src/components/contact/ContactLeft.jsx
+++ src/components/contact/ContactLeft.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { contactImg } from '../../assets/index.jsx'
+import { FaLinkedin } from 'react-icons/fa'
 
 const ContactLeft = () => {
   return (
@@ -10,10 +11,30 @@ const ContactLeft = () => {
       <img 
         className="w-full h-64 object-cover rounded-lg mb-2"
       src={contactImg} alt="contactImg" />
-      <div>
-
+      <div className='flex flex-col gap-4'>
+           <h3 className="text-3xl font-bold text-white">Samsul Alam</h3>
+        <p className="text-lg font-normal text-gray-400">Front-End-Developer</p>
+        <p className="text-base text-gray-400 tracking-wide">
+          Proficient front-end developer with a passion for immersive digital
+          experiences and a solid foundation in HTML, CSS, JavaScript, React,TailWind CSS,MUI
+          Redux, and Nodejs
+        </p>
+        <p className="text-base text-gray-400 flex items-center gap-2">
+          Phone: <span className="text-lightText">+91 8167786956</span>
+        </p>
+        <p className="text-base text-gray-400 flex items-center gap-2">
+          Email: <span className="text-lightText">samsul7718@gmail.com</span>
+        </p>
       </div>
       <div>
+           <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
+           <div>
+            <span>
+              <a href="https://www.linkedin.com/in/samsul-alam-427a0a19a/">
+                 <FaLinkedin />
+              </a>
+            </span>
+           </div>
 
       </div>
     </div>
